6a63c3e66526a43e8491139a82ce74f372c5a6bb
Soumyadeep Mallick <soumyadeep@maventic.com>
2026-03-24 16:12:55 +0000

    Computed Criticality in service implementation

--- srv/event-service.js
+++ srv/event-service.js
@@ -2,7 +2,18 @@ const cds = require('@sap/cds');
 
 module.exports = cds.service.impl((srv) => {
     srv.after('READ', 'Event', (data) => {
+        const events = Array.isArray(data) ? data : [data];
         const today = new Date();
+
+        events.forEach(e => {
+            if (e.budget > 500) {
+                e.criticality = 3; 
+            } else if (e.budget > 250) {
+                e.criticality = 2; 
+            } else {
+                e.criticality = 1; 
+            }
+        });
 
         data.forEach(e => {
             if (e.eventDate) {
